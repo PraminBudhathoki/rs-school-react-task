d38236fbdf13897b626f57dfa12addf7118a8c69
PraminBudhathoki <budhathokipramin@protonmail.com>
2025-02-08 21:27:31 +0000

    feat: add Pagination component after searching results
    - Pagination component added for the task.
    - integrated pagination component with app component and updated the
      fetching using useCallback.
    - updated api.ts for required output.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import SearchResults from './components/SearchResults/SearchResults';
 import { fetchCharacters } from './services/api';
 import { Character } from './interfaces/types';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import TopField from './components/TopField/TopField';
 import ErrorButton from './components/ErrorButton/ErrorButton';
+import Pagination from './components/Pagination/Pagination';
 
 const App: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState<string>(
@@ -13,17 +15,17 @@ const App: React.FC = () => {
   const [results, setResults] = useState<Character[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+  const [totalPages, setTotalPages] = useState<number>(1);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = parseInt(searchParams.get('page') || '1', 10);
 
-  useEffect(() => {
-    fetchResults(searchTerm);
-  }, [searchTerm]);
-
-  const fetchResults = async (term: string) => {
+  const fetchResults = useCallback(async (term: string, page: number) => {
     setLoading(true);
     setError(null);
     try {
-      const data = await fetchCharacters(term);
-      setResults(data);
+      const { characters, totalPages } = await fetchCharacters(term, page);
+      setResults(characters);
+      setTotalPages(totalPages);
       setLoading(false);
     } catch (error: unknown) {
       if (error instanceof Error) {
@@ -32,10 +34,20 @@ const App: React.FC = () => {
         setError(error.message);
       }
     }
-  };
+  }, []);
+
+  useEffect(() => {
+    fetchResults(searchTerm, page);
+  }, [fetchResults, searchTerm, page]);
 
   const handleSearch = (term: string) => {
     setSearchTerm(term);
+    fetchResults(term, 1);
+    setSearchParams({ page: '1' });
+  };
+
+  const handlePageChange = (newPage: number) => {
+    setSearchParams({ page: newPage.toString() });
   };
 
   return (
@@ -47,6 +59,12 @@ const App: React.FC = () => {
         </div>
         <div className="results-section">
           <SearchResults results={results} loading={loading} />
+          <Pagination
+            currentPage={page}
+            totalPages={totalPages}
+            onPageChange={handlePageChange}
+            hasResults={results.length > 0}
+          />
           <ErrorButton />
         </div>
       </div>
